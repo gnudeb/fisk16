63aece87733dcfa1b17f505ddf1dfbddddf8fa0c
gnudeb <gnudeb0@gmail.com>
2018-06-23 21:20:21 +0300

    Swap register nibbles in register byte of each instruction (mov r5, r3 is now 0x0053)

--- fisk16/instructions.py
+++ fisk16/instructions.py
@@ -24,7 +24,7 @@ def as_nibbles(byte):
 
 
 def r16_r16(cpu):
-    source_id, target_id = as_nibbles(cpu.ram[cpu.ip])
+    target_id, source_id = as_nibbles(cpu.ram[cpu.ip])
     target_ptr = Pointer(cpu.register_ram, target_id*2, 2)
     source_ptr = Pointer(cpu.register_ram, source_id*2, 2)
     cpu.ip += 1
@@ -32,7 +32,7 @@ def r16_r16(cpu):
 
 
 def r16_ind(cpu):
-    source_id, target_id = as_nibbles(cpu.ram[cpu.ip])
+    target_id, source_id = as_nibbles(cpu.ram[cpu.ip])
     target_ptr = Pointer(cpu.register_ram, target_id * 2, 2)
     pointer = cpu.register_ram.read(source_id*2, 2)
     source_ptr = Pointer(cpu.ram, pointer, 2)
@@ -41,7 +41,7 @@ def r16_ind(cpu):
 
 
 def ind_r16(cpu):
-    source_id, target_id = as_nibbles(cpu.ram[cpu.ip])
+    target_id, source_id = as_nibbles(cpu.ram[cpu.ip])
     source_ptr = Pointer(cpu.register_ram, source_id * 2, 2)
     pointer = cpu.register_ram.read(target_id*2, 2)
     target_ptr = Pointer(cpu.ram, pointer, 2)
@@ -50,7 +50,7 @@ def ind_r16(cpu):
 
 
 def r8_r8(cpu):
-    source_id, target_id = as_nibbles(cpu.ram[cpu.ip])
+    target_id, source_id = as_nibbles(cpu.ram[cpu.ip])
     target_ptr = Pointer(cpu.register_ram, target_id, 1)
     source_ptr = Pointer(cpu.register_ram, source_id, 1)
     cpu.ip += 1
@@ -58,7 +58,7 @@ def r8_r8(cpu):
 
 
 def r8_ind(cpu):
-    source_id, target_id = as_nibbles(cpu.ram[cpu.ip])
+    target_id, source_id = as_nibbles(cpu.ram[cpu.ip])
     target_ptr = Pointer(cpu.register_ram, target_id, 1)
     pointer = cpu.register_ram.read(source_id*2, 2)
     source_ptr = Pointer(cpu.ram, pointer, 1)
@@ -67,7 +67,7 @@ def r8_ind(cpu):
 
 
 def ind_r8(cpu):
-    source_id, target_id = as_nibbles(cpu.ram[cpu.ip])
+    target_id, source_id = as_nibbles(cpu.ram[cpu.ip])
     source_ptr = Pointer(cpu.register_ram, source_id, 1)
     pointer = cpu.register_ram.read(target_id*2, 2)
     target_ptr = Pointer(cpu.ram, pointer, 1)
@@ -76,7 +76,7 @@ def ind_r8(cpu):
 
 
 def r16_imm16(cpu):
-    _, target_id = as_nibbles(cpu.ram[cpu.ip])
+    target_id, _ = as_nibbles(cpu.ram[cpu.ip])
     target_ptr = Pointer(cpu.register_ram, target_id*2, 2)
 
     cpu.ip += 1
@@ -90,7 +90,7 @@ def r16_imm16(cpu):
 
 
 def r8_imm8(cpu):
-    _, target_id = as_nibbles(cpu.ram[cpu.ip])
+    target_id, _ = as_nibbles(cpu.ram[cpu.ip])
     target_ptr = Pointer(cpu.register_ram, target_id, 1)
 
     cpu.ip += 1
--- fisk16_test.py
+++ fisk16_test.py
@@ -9,14 +9,14 @@ class Fisk16InstructionsTests(unittest.TestCase):
         self.cpu = Fisk16()
 
     def test_mov_r16_r16(self):
-        self.cpu.ram.write(0, 0x0001, 2)    # mov r1, r0
+        self.cpu.ram.write(0, 0x0010, 2)    # mov r1, r0
         self.cpu.register_ram.write(0, 0xbeef, 2)  # r0 = 0xbeef
 
         self.cpu.tick()
         self.assertEqual(self.cpu.register_ram.read(2, 2), 0xbeef)
 
     def test_r16_ind(self):
-        self.cpu.ram.write(0, 0x0101, 2)    # mov r1, [r0]
+        self.cpu.ram.write(0, 0x0110, 2)    # mov r1, [r0]
         self.cpu.ram.write(8, 0xbeef, 2)
         self.cpu.register_ram.write(0, 8, 2)  # r0 = 8
 
@@ -24,7 +24,7 @@ class Fisk16InstructionsTests(unittest.TestCase):
         self.assertEqual(self.cpu.register_ram.read(2, 2), 0xbeef)
 
     def test_ind_r16(self):
-        self.cpu.ram.write(0, 0x0201, 2)    # mov [r1], r0
+        self.cpu.ram.write(0, 0x0210, 2)    # mov [r1], r0
         self.cpu.register_ram.write(0, 0xbeef, 2)
         self.cpu.register_ram.write(2, 8, 2)  # r1 = 8
 
@@ -32,14 +32,14 @@ class Fisk16InstructionsTests(unittest.TestCase):
         self.assertEqual(self.cpu.ram.read(8, 2), 0xbeef)
 
     def test_r8_r8(self):
-        self.cpu.ram.write(0, 0x0301, 2)    # mov r0h, r0l
+        self.cpu.ram.write(0, 0x0310, 2)    # mov r0h, r0l
         self.cpu.register_ram.write(0, 0xa5)
 
         self.cpu.tick()
         self.assertEqual(self.cpu.register_ram.read(1), 0xa5)
 
     def test_r8_ind(self):
-        self.cpu.ram.write(0, 0x0410, 2)      # mov r0l, [r1]
+        self.cpu.ram.write(0, 0x0401, 2)      # mov r0l, [r1]
         self.cpu.register_ram.write(2, 8, 2)  # r1 = 8
         self.cpu.ram.write(8, 0xa5)
 
@@ -47,7 +47,7 @@ class Fisk16InstructionsTests(unittest.TestCase):
         self.assertEqual(self.cpu.register_ram.read(0), 0xa5)
 
     def test_ind_r8(self):
-        self.cpu.ram.write(0, 0x0501, 2)      # mov [r1], r0l
+        self.cpu.ram.write(0, 0x0510, 2)      # mov [r1], r0l
         self.cpu.register_ram.write(2, 8, 2)  # r1 = 8
         self.cpu.register_ram.write(0, 0xa5)  # r0l = 0xa5
 
