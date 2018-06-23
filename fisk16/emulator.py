b3751e9952207c07773d4a6bcccda06f154d67b8
gnudeb <gnudeb0@gmail.com>
2018-06-23 21:48:50 +0300

    Implement named RAM for easy testing

--- fisk16/emulator.py
+++ fisk16/emulator.py
@@ -1,5 +1,6 @@
 from .instruction_set import instruction_set
 
+
 class RAM(bytearray):
 
     def __str__(self):
@@ -21,19 +22,70 @@ class RAM(bytearray):
             value >>= 8
 
 
+class RegisterRAM(RAM):
+
+    def __init__(self):
+        # Fisk16 has 32 word-sized registers = 64 bytes
+        super().__init__(32 * 2)
+        self.pointers = {
+            'r0':  (0 * 2, 2),
+            'r1':  (1 * 2, 2),
+            'r2':  (2 * 2, 2),
+            'r3':  (3 * 2, 2),
+            'r4':  (4 * 2, 2),
+            'r5':  (5 * 2, 2),
+            'r6':  (6 * 2, 2),
+            'r7':  (7 * 2, 2),
+            'r8':  (8 * 2, 2),
+            'r9':  (9 * 2, 2),
+            'r10': (10 * 2, 2),
+            'r11': (11 * 2, 2),
+            'r12': (12 * 2, 2),
+            'r13': (13 * 2, 2),
+            'r14': (14 * 2, 2),
+            'ip':  (15 * 2, 2),
+            'r0l': (0, 1),
+            'r0h': (1, 1),
+            'r1l': (2, 1),
+            'r1h': (3, 1),
+            'r2l': (4, 1),
+            'r2h': (5, 1),
+            'r3l': (6, 1),
+            'r3h': (7, 1),
+            'r4l': (8, 1),
+            'r4h': (9, 1),
+            'r5l': (10, 1),
+            'r5h': (11, 1),
+            'r6l': (12, 1),
+            'r6h': (13, 1),
+            'r7l': (14, 1),
+            'r7h': (15, 1),
+        }
+
+    def read(self, address, size=1):
+        if type(address) is str:
+            address, size = self.pointers[address]
+        return super().read(address, size)
+
+    def write(self, address, value, size=1):
+        if type(address) is str:
+            address, size = self.pointers[address]
+        super().write(address, value, size)
+
+
 class Fisk16:
 
     def __init__(self):
-        self.register_ram = RAM(32 * 2)
+        self.register_ram = RegisterRAM()
         self.ram = RAM(32)
 
     @property
     def ip(self):
-        return self.register_ram.read(30, 2)
+        return self.register_ram.read('ip')
 
     @ip.setter
     def ip(self, value):
-        self.register_ram.write(30, value, 2)
+        self.register_ram.write('ip', value)
 
     @staticmethod
     def _as_nibbles(byte):
--- fisk16_test.py
+++ fisk16_test.py
@@ -10,46 +10,46 @@ class Fisk16InstructionsTests(unittest.TestCase):
 
     def test_mov_r16_r16(self):
         self.cpu.ram.write(0, 0x0010, 2)    # mov r1, r0
-        self.cpu.register_ram.write(0, 0xbeef, 2)  # r0 = 0xbeef
+        self.cpu.register_ram.write('r0', 0xbeef)
 
         self.cpu.tick()
-        self.assertEqual(self.cpu.register_ram.read(2, 2), 0xbeef)
+        self.assertEqual(self.cpu.register_ram.read('r1'), 0xbeef)
 
     def test_r16_ind(self):
         self.cpu.ram.write(0, 0x0110, 2)    # mov r1, [r0]
         self.cpu.ram.write(8, 0xbeef, 2)
-        self.cpu.register_ram.write(0, 8, 2)  # r0 = 8
+        self.cpu.register_ram.write('r0', 8)  # r0 = 8
 
         self.cpu.tick()
-        self.assertEqual(self.cpu.register_ram.read(2, 2), 0xbeef)
+        self.assertEqual(self.cpu.register_ram.read('r1'), 0xbeef)
 
     def test_ind_r16(self):
         self.cpu.ram.write(0, 0x0210, 2)    # mov [r1], r0
-        self.cpu.register_ram.write(0, 0xbeef, 2)
-        self.cpu.register_ram.write(2, 8, 2)  # r1 = 8
+        self.cpu.register_ram.write('r0', 0xbeef)
+        self.cpu.register_ram.write('r1', 8)
 
         self.cpu.tick()
         self.assertEqual(self.cpu.ram.read(8, 2), 0xbeef)
 
     def test_r8_r8(self):
         self.cpu.ram.write(0, 0x0310, 2)    # mov r0h, r0l
-        self.cpu.register_ram.write(0, 0xa5)
+        self.cpu.register_ram.write('r0l', 0xa5)
 
         self.cpu.tick()
-        self.assertEqual(self.cpu.register_ram.read(1), 0xa5)
+        self.assertEqual(self.cpu.register_ram.read('r0h'), 0xa5)
 
     def test_r8_ind(self):
         self.cpu.ram.write(0, 0x0401, 2)      # mov r0l, [r1]
-        self.cpu.register_ram.write(2, 8, 2)  # r1 = 8
+        self.cpu.register_ram.write('r1', 8)  # r1 = 8
         self.cpu.ram.write(8, 0xa5)
 
         self.cpu.tick()
-        self.assertEqual(self.cpu.register_ram.read(0), 0xa5)
+        self.assertEqual(self.cpu.register_ram.read('r0l'), 0xa5)
 
     def test_ind_r8(self):
         self.cpu.ram.write(0, 0x0510, 2)      # mov [r1], r0l
-        self.cpu.register_ram.write(2, 8, 2)  # r1 = 8
-        self.cpu.register_ram.write(0, 0xa5)  # r0l = 0xa5
+        self.cpu.register_ram.write('r1', 8)
+        self.cpu.register_ram.write('r0l', 0xa5)
 
         self.cpu.tick()
         self.assertEqual(self.cpu.ram.read(8), 0xa5)
@@ -58,13 +58,13 @@ class Fisk16InstructionsTests(unittest.TestCase):
         self.cpu.ram.write(0, 0x0600beef, 4)  # mov r0, 0xbeef
 
         self.cpu.tick()
-        self.assertEqual(self.cpu.register_ram.read(0, 2), 0xbeef)
+        self.assertEqual(self.cpu.register_ram.read('r0'), 0xbeef)
 
     def test_r8_imm8(self):
         self.cpu.ram.write(0, 0x0700a5, 3)  # mov r0l, 0xa5
 
         self.cpu.tick()
-        self.assertEqual(self.cpu.register_ram.read(0), 0xa5)
+        self.assertEqual(self.cpu.register_ram.read('r0l'), 0xa5)
 
 
 if __name__ == "__main__":
