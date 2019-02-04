915452c844bf7e322bf03c7fd1053340ef4ee83a
gnudeb <gnudeb0@gmail.com>
2019-02-04 16:12:12 +0200

    Add docstring for `Instruction` class

--- fisk16/instruction.py
+++ fisk16/instruction.py
@@ -1,5 +1,26 @@
 
 class Instruction:
+    """
+    A Fisk 16 instruction.
+
+    This class is a wrapper around 16-bit value, that provides a convenient
+    way to read and write various bit fields.
+
+    For example, highest 4 bits (from 12 to 15, inclusive) are defined by
+    Fisk 16 specification to store the value of opcode:
+
+    >>> i = Instruction(0b1001_0000_0000_0000)
+    >>> print(bin(i.opcode))
+    0b1001
+
+    You can also manually subscript `Instruction` objects:
+
+    >>> i = Instruction(0b1001_0000_0000_0000)
+    >>> print(bin(i[12:15]))
+    0b1001
+    >>> print(bin(i[15:12]))    # Bit boundaries are reversed
+    0b1001
+    """
 
     def __init__(self, value: int):
         self.value = value
